9ade96771c6e3f07cc51f90a2351a8b92db4feed
rusgyn <rusgynmagsanay@gmail.com>
2025-01-17 15:46:59 -0500

    Modify and create routes to use.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,51 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminLogin from './components/admin_account/AdminLogin';
-import './styles/App.scss';
+import Dashboard from './components/Dashboard';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div className='App'>
-        <AdminLogin />
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={ <AdminLogin />} />
+          <Route path="/dashboard" element={ <Dashboard /> } />
+        </Routes>
       </div>
-    </>
-  )
+
+    </Router>
+  );
 }
 
 export default App;
-
-
-    
-
-      {/* 
-      
-      // import { useState } from 'react'
-      // import reactLogo from './assets/react.svg'
-      // import viteLogo from '/vite.svg'
-
-        
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="App">
-        Click on the Vite and React logos to learn more
-      </p>
- */}
-
-
-
-      
