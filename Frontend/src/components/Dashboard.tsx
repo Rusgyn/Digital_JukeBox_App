9faa3d6e0cc3ce6c5ca2161a788920d85206fd7a
rusgyn <rusgynmagsanay@gmail.com>
2025-01-17 22:02:27 -0500

    Create Dashboard styling

--- Frontend/src/components/Dashboard.tsx
+++ Frontend/src/components/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import '../styles/Dashboard.scss';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -14,14 +15,20 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <h2>Welcome to the Dashboard!</h2>
-      <button 
-        type='button'
-        value="Admin Login"
-        onClick={handleAdminLoginNavigation}></button>
-      <button 
-        type='button'
-        value="Main"
-        onClick={handleMainNavigation}></button>
+      <p>Click on the buttons below to navigate to different pages.</p>
+
+      <div className="dashboard__content">
+        <button
+          className="dashboard__content__main_button"
+          type='button'
+          value="Main"
+          onClick={handleMainNavigation}>Main</button>
+        <button 
+          className="dashboard__content__admin-login_button"
+          type='button'
+          value="Admin Login"
+          onClick={handleAdminLoginNavigation}>Login</button>
+      </div>
     </div>
   );
 }
