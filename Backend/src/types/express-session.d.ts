69dab44a96d5a501373de38ab81bbc75db391ede
rusgyn <rusgynmagsanay@gmail.com>
2025-01-27 21:36:37 -0500

    Extend the express-session session data with custom property for TypeScript to know.

---
+++ Backend/src/types/express-session.d.ts
@@ -0,0 +1,15 @@
+import session from "express-session";
+
+// Extend the existing session data interface
+declare module 'express-session' { 
+  interface SessionData {
+    loggedAdminUser: { id: number };
+    // Can define more custom property here
+  }
+}
+
+/**
+ * NOTE:
+ * By default, TypeScript uses the built-in types for express-session, and it does not include your custom properties.
+ * Hence, you need to extend the SessionData interface provided by the express-session library to include your custom property (example: loggedAdminUser).
+ */
