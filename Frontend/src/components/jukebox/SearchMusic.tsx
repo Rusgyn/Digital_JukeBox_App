a4dfd59e8242e34bdbfa8d0e98f9135610711f10
rusgyn <rusgynmagsanay@gmail.com>
2025-02-03 21:08:04 -0500

    Add input field to test the Search:

--- Frontend/src/components/jukebox/SearchMusic.tsx
+++ Frontend/src/components/jukebox/SearchMusic.tsx
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
+
 const SearchMusic = () => {
+
+  const [findMusic, setFindMusic] = useState("");
+
+  const handleSearch = async(e: React.FormEvent) => {
+    e.preventDefault();
+    alert("You Click Search!");
+  }
 
   return (
     <div>
       <h2> This is SearchMusic Component </h2>
+      <div>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Search here ..."
+            value={findMusic}
+            onChange={(e) => setFindMusic(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
     </div>
   );
 };
