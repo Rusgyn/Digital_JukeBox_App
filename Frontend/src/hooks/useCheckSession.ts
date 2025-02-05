4e2db80728c1051daa9990638bc18e6f8718da9c
rusgyn <rusgynmagsanay@gmail.com>
2025-02-05 16:18:14 -0500

    Define a function that check session

---
+++ Frontend/src/hooks/useCheckSession.ts
@@ -0,0 +1,34 @@
+// hooks
+
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useCheckSession = () => {
+  const navigate = useNavigate();
+  const [isSessionChecked, setIsSessionChecked] = useState(false); //control rendering
+
+  useEffect(() => {
+    const checkSession = async() => {
+      try {
+        const response = await axios.get('/jukeBox/check-session', { withCredentials: true }); 
+
+        if (!response.data.loggedIn) {
+          navigate('/admin-login', { replace: true } );
+          return;
+        }
+
+      } catch (error) {
+        console.error("Error checking session Frontend: ", error);
+        navigate('/admin-login', { replace: true } );
+        return;
+      }
+      setIsSessionChecked(true); // Mark session is checked
+    }
+    
+    checkSession();
+  }, [navigate]);
+
+};
+
+export default useCheckSession;
