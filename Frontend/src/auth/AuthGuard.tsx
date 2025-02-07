281e027e6021ba1f1ad10cc85432054502ffd282
rusgyn <rusgynmagsanay@gmail.com>
2025-02-07 14:17:28 -0500

    Fix the AuthGuard file

--- Frontend/src/auth/AuthGuard.tsx
+++ Frontend/src/auth/AuthGuard.tsx
@@ -4,16 +4,37 @@ import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import axios from "axios";
 
-const AuthGuard = () => {
+const AuthGuard = () => { // if export const AuthGuard, => import { AuthGuard}
   const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-}
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const response = await axios.get('/jukeBox/check-session', {withCredentials: true});
+        console.log("Auth Guard response: ", response.data);
+        setIsAuthenticated(response.data.loggedIn)
+      } catch (error) {
+        console.log('Error validating the session: ', error);
+        setIsAuthenticated(false);
+      }
+      setIsLoading(false);
+    };
+    checkSession();
+  }, []); // [], runs only once
 
-export default AuthGuard;
+  if (isLoading) return null; // This prevent rendering anything until session check completes
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/admin-login" replace />;
+
+};
+
+export default AuthGuard; // => import AuthGuard
 
 //NOTE:
 
 /**
  * { withCredentials: true } = it is a configuration option provided by the axios library. A configuration option used to include credentials in cross-site requests, ensuring that authentication and session management work correctly
+ * 
+ * <Outlet /> = react built in component, placeholder for pages that match a route.
  */
