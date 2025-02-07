c15f28f75c7878fe23af5cbb9f19cd42389266b0
rusgyn <rusgynmagsanay@gmail.com>
2025-02-07 14:24:48 -0500

    Wrap the routes that requires session, the protected routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from './components/Dashboard';
 import Main from './components/Main';
 import AdminRegister from './components/admin_account/AdminRegister'; 
 import SearchMusic from './components/jukebox/SearchMusic';
+import AuthGuard from './auth/AuthGuard';
 import './styles/App.scss';
 
 const App = () => {
@@ -11,16 +12,22 @@ const App = () => {
     <Router>
       <div>
         <Routes>
+          {/* No session required. This Route is public */}
           <Route path="/" element={ <Main />} />
-          <Route path="/dashboard" element={ <Dashboard /> } />
           <Route path="/admin-login" element= { <AdminLogin /> } />
-          <Route path='/admin-register' element={ <AdminRegister /> } />
-          <Route path='/media-search' element={ <SearchMusic /> } />
+          
+          {/* Session Required. Protected Route in one wrapper*/}
+          <Route element={<AuthGuard />}>
+            <Route path="/dashboard" element={ <Dashboard /> } />
+            <Route path='/admin-register' element={ <AdminRegister /> } />
+            <Route path='/media-search' element={ <SearchMusic /> } />
+          </Route>
+          
         </Routes>
       </div>
 
     </Router>
   );
-}
+};
 
 export default App;
