4344bfef6e574cf954f7c53e6164e1ff9edfcdd5
rusgyn <rusgynmagsanay@gmail.com>
2025-02-13 15:19:46 -0500

    Define additional interface for all songs

--- Backend/src/types/jukeBox/PlaylistTypes.ts
+++ Backend/src/types/jukeBox/PlaylistTypes.ts
@@ -11,3 +11,22 @@ export interface SelectedSong {
   id: number; //external API id number from Search result
   title: string;
 };
+
+export interface PlaylistItem {
+  id: number;
+  song_external_id: bigint;
+  title: string;
+  song_like: number;
+  preview: string, // song audio preview
+  duration: number, // song length
+  artist: string, // artist name
+  album: string, //cover name
+  album_cover_small: string, //cover image
+  album_cover_medium: string, //cover image
+  album_cover_big: string //cover image
+};
+
+export interface FavoriteSong {
+  id: number,
+  title: string
+};
