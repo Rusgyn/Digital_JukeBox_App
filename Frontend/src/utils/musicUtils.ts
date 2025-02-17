dce7f7360b8237a2056c39e98591183b07e8b6da
rusgyn <rusgynmagsanay@gmail.com>
2025-02-16 23:16:47 -0500

    Modify the statement condition that sort the tracks as per number of likes

--- Frontend/src/utils/musicUtils.ts
+++ Frontend/src/utils/musicUtils.ts
@@ -1,7 +1,7 @@
 // music-related utilities
 
 import SearchMusicResult from "../../../Backend/src/types/jukeBox/searchMusicResultTypes";
-import { Playlist } from "../../../Backend/src/types/jukeBox/playlistTypes";
+import { PlaylistItem } from "../../../Backend/src/types/jukeBox/playlistTypes";
 
 // Sorted Music Tracks by title
 export const sortTracksByTitle = (music: SearchMusicResult[]) => {
@@ -9,13 +9,14 @@ export const sortTracksByTitle = (music: SearchMusicResult[]) => {
 };
 
 // Sorted Music by number of Likes, or created_at
-export const sortTracksByLike = (music: Playlist[]) => {
+export const sortTracksByLike = (music: PlaylistItem[]) => {
   return music.sort((a, b) => {
     if (b.song_like !== a.song_like) {
       return b.song_like - a.song_like; // Sort DESC
     }
     // If song_like is equal, then sort by created_at ASC.
-    return a.created_at.getTime() - b.created_at.getTime();
+    //return a.created_at.getTime() - b.created_at.getTime();
+    return a.id - b.id;
     //getTime() ensure to have an accurate comparison of time
   })
 };
